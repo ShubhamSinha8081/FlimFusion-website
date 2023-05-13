afc098d992f4c518b0aede30aaf663994214a9e3
shubham sinha <shubhamsinha8081@gmail.com>
2023-05-14 02:30:49 +0530

    sample commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const menuBtn = document.querySelector(".menu");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    
+});
+
+slides.forEach((slide) => {
+    slide.classList.remove("active");
+});
+
+contents.forEach((content) => {
+    content.classList.remove("active");
+});
+
+btns[manual].classList.add("active");
+slides[manual].classList.add("active");
+contents[manual].classList.add("active");
+}
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+$(document).ready(function() {
+    $('#autoWidth,#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth,#autoWidth2').removeClass('cS-hidden');
+        } 
+    });  
+  });
